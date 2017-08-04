ed7abd47033e9775dbbd54dce258ae90f1cb4102
Leigh Steiner <steinerleigh@gmail.com>
2017-08-04 14:54:44 -0400

    ready for first tests

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const authMaster = require('auth-master');
 const userAuthenticator = new authMaster(db.Users, {user: function(id){ validationHere }, siteController: function(){...}, admin: function(){}});
 // userAuthenticator.isAuthorized(userId);
 
+app.use(userAuthenticator.checkAuthorizations) //thus only receives req res next
+//dependent on PASSPORT because then you check for req.user 
 
+function volleyball(req, res, next){
+
+}
 // the goal:  to create a library that works without Express or Sequelize, 
 //takes in a model to authenticate, a series of authorization functions
 // and performs security functions 
@@ -24,37 +29,75 @@ function authMaster(){
 			constructor(modelAuthenticator, authObject){
 				this.id = secretId++
 				secretLocation[this.id] = {
-					authObject: authObject
+					authObject: authObject, 
+					authFailLog : {}
 				};
-				this.modelAuthenticator = modelAuthenticator;
-				this.authFunctions = authObject || {
-					async isUser : function(id){
-						let user = await this.modelAuthenticator.findById(id)
+				this.checkAuthorizations = this.checkAuthorizations.bind(this);
+				this.authFailLogger = this.authFailLogger.bind(this);
 
+				this.modelAuthenticator = modelAuthenticator;
+				this.authFunctions = authObject || {  //can we contain the Sequelize in the authObj? 
+					 isUser : async function(id){
+						let user = await this.modelAuthenticator.findById(id)
+						user ? true : false; 
 					}, 
-					isThisUser : function(id){},
-					isMod : function(id){},
-					isAdmin: function(id){},
-					isSiteController : function(id){}
+					// isThisUser : async function(id, targetId){},  //this doesn't work because it doesn't match
+					isMod : async function(id){
+						let user = await this.modelAuthenticator.findById(id)
+						user.isMod ? true : false; 
+
+					},
+					isAdmin: async function(id){
+						let user = await this.modelAuthenticator.findById(id)
+						user.isAdmin ? true : false; 
+					},
+					isSiteController : async function(id){
+						let user = await this.modelAuthenticator.findById(id)
+						user.isSiteController ? true : false ;
+					} //make DRYer 
+					// isParticularClearance(id, clearanceStr){} //is this a better check ModelAuthenticator[clearanceStr]: true
 				}
 			}
 
-			 checkAuthorizations(userId){
+			 checkAuthorizations(req,res,next){ // is now a piece of middleware which relies on PASSPORT 
 			 	let output = [];
-			 	for (let k in secretLocation[this.id].authFunctions){
-			 		if (authFunctions[k](userId)){
-			 			output.push(k);
-			 		}
-			 		return output;
+			 	if(req.user){
+				 		for (let k in secretLocation[this.id].authFunctions){
+					 		if (authFunctions[k](req.user.id)){
+					 			output.push(k);
+					 		}
+				 		req.user.clearances = output;
+				 		next();
+			 		} //check if req.user.clearances.contains(x)
+			 	}else{
+			 		throw new Error('checkAuth: user is not logged in')
 			 	}
 			 }
-			 checkOneAuth(userId, whichAuth){
-				if (secretLocation[this.id].authFunction.hasOwnProperty(whichAuth)){
-					return whichAuth(userId); 
-				}
-			}
+
+			 authFailLogger(whichAuth){  //RETURNS a function which is a piece of middleware 
+			 	return function(req,res,next){
+				 	if (req.user){
+				 		if(secretLocation[this.id].authFunction.hasOwnProperty(whichAuth)){
+				 			if (req.user.clearances.includes(whichAuth)){
+				 				next();
+				 			}else{
+				 				if (secretLocation[this.id].authFailLog[whichAuth]){
+				 					secretLocation[this.id].authFailLog[whichAuth].push(req.user.id);
+				 				}else{
+				 					secretLocation[this.id].authFailLog[whichAuth] = [req.user.id];
+				 				}
+				 			}
+				 		}else{
+				 			throw new Error('not a valid authorization check');
+				 		}
+				 	}
+			 	}else{
+			 		throw new Error('authFailLog: user is not logged in');
+			 	}	
+			 }
+			
+			
 		}
 	}
-
 	)();
 }
--- scratchwork.js
+++ scratchwork.js
@@ -32,3 +32,33 @@ function authMaster(){
     })();
     
 }
+
+//  checkOneAuth(userId, whichAuth){  //can't be a piece of middleware  -- this should be refactored to be our attempt log
+            //  if (secretLocation[this.id].authFunction.hasOwnProperty(whichAuth)){
+            //      return whichAuth(userId); 
+            //  }
+            // }
+/*attempt clearance log notes:  
+
+so, the path is this: you run thru the check Authorizations middleware, which attaches a clearances property (an array)
+to the req.user (PASSPORT DEPENDENT)
+
+later on, a route runs a function where it checks to see if req.user.clearances INCLUDES a given clearance
+IF the answer is no, that userId gets added to a log of users who incorrectly attempt to pass a given clearance 
+log model :
+
+FailedClearanceLog = {
+    Mod: [1,57,1004,6,327], level 2 ?
+    Admin: [899,4,1988,66],  level 5 ?
+    SiteController: [12, 10087] level 10 ?
+}
+
+Options: require that "clearance levels" conform to a given set of numbers and only are translated into meaningful strings inside the function 
+1-10 are options, which is probably more clearance levels than anyone needs, which should be fine  
+
+Remaining Questions:  
+where does the clearance log live? 
+can it live in the private object?  
+how do we get the clearance level IN to the function, if it's middleware 
+
+*/
